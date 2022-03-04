e60796bf6f0b35609e9ce7a2dfac8dc8fcfe528e
Cleve Stuart <cleve.stuart@fauna.com>
2022-03-04 14:48:08 -0800

    Add more testing to more fully show and prove the library's contract

--- test/index.spec.js
+++ test/index.spec.js
@@ -18,6 +18,15 @@ test('named export | convert string "2" to integer 2', () => {
   expect(result).toBe(expected);
 });
 
+test('named export | convert string " 2 " to integer 2', () => {
+  const input = ' 2 ';
+  const expected = 2;
+
+  const result = convertStringToNumber(input);
+
+  expect(result).toBe(expected);
+});
+
 test('named export | convert string "2.2" to float 2.2', () => {
   const input = '2.2';
   const expected = 2.2;
@@ -27,8 +36,44 @@ test('named export | convert string "2.2" to float 2.2', () => {
   expect(result).toBe(expected);
 });
 
+test('named export | convert string " 2.2 " to float 2.2', () => {
+  const input = ' 2.2 ';
+  const expected = 2.2;
+
+  const result = convertStringToNumber(input);
+
+  expect(result).toBe(expected);
+});
+
 test('named export | convert string "franz" to NaN', () => {
   const input = 'franz';
+  const expected = NaN;
+
+  const result = convertStringToNumber(input);
+
+  expect(result).toBe(expected);
+});
+
+test('named export | convert string "" to NaN', () => {
+  const input = '';
+  const expected = NaN;
+
+  const result = convertStringToNumber(input);
+
+  expect(result).toBe(expected);
+});
+
+test('named export | convert string " " to NaN', () => {
+  const input = ' ';
+  const expected = NaN;
+
+  const result = convertStringToNumber(input);
+
+  expect(result).toBe(expected);
+});
+
+test('named export | convert string "3.352.2" to NaN', () => {
+  const input = '3.352.2';
   const expected = NaN;
 
   const result = convertStringToNumber(input);
@@ -51,3 +96,29 @@ test('named export | throw because wrong value type (undefined)', () => {
 
   expect(result).toThrow('value has to be typeof: \'string\' but got typeof: \'undefined\'');
 });
+
+
+test('named export | throw because wrong value type (undefined)', () => {
+  const input = undefined;
+
+  const result = () => convertStringToNumber(input);
+
+  expect(result).toThrow('value has to be typeof: \'string\' but got typeof: \'undefined\'');
+});
+
+test('named export | throw because wrong value type (object)', () => {
+  const input = {};
+
+  const result = () => convertStringToNumber(input);
+
+  expect(result).toThrow('value has to be typeof: \'string\' but got typeof: \'object\'');
+});
+
+test('named export | null input throws because wrong value type (object)', () => {
+  const input = null;
+
+  const result = () => convertStringToNumber(input);
+
+  expect(result).toThrow('value has to be typeof: \'string\' but got typeof: \'object\'');
+});
+
